71dfa0277ca1960626b79f254fe16362697b8b05
benj9000 <49900294+benj9000@users.noreply.github.com>
2024-10-05 18:20:58 +0200

    Add template for README and LICENSE skeletons

--- flake.nix
+++ flake.nix
@@ -9,6 +9,18 @@
         description = "A flake skeleton with flake-utils for all default systems";
       };
 
+      readme-and-licence = {
+        path = ./readme-and-licence;
+        description = "Basic README and LICENSE skeletons";
+        welcomeText = ''
+          # Initialized basic README.md and LICENSE.md skeletons.
+
+          - Choose a license (compare <https://choosealicense.com/>),
+          - update the `Licence` section in `README.md`, and
+          - add the licence text to `LICENSE.md`.
+        '';
+      };
+
     };
   };
 }
---
+++ readme-and-licence/README.md
@@ -0,0 +1,23 @@
+# My project
+
+…
+
+
+## Installation
+
+…
+
+
+## Usage
+
+…
+
+
+## Contributing
+
+…
+
+
+## Licence
+
+Distributed under the [<licence shorthand>](https://example.com) licence. See [LICENSE.md](./LICENSE.md) for more information.
