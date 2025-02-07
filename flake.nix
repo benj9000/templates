2d48d44e8c49897717b7196de84ee9b6e1254d0a
benj9000 <49900294+benj9000@users.noreply.github.com>
2025-02-08 00:05:07 +0100

    Order templates in flake.nix alphabetically

--- flake.nix
+++ flake.nix
@@ -9,28 +9,6 @@
         description = "A flake skeleton with flake-utils for all default systems";
       };
 
-      revealjs = {
-        path = ./revealjs;
-        description = "A reveal.js presentation";
-        welcomeText = ''
-          # Initialized a reveal.js presentation.
-
-          reveal.js: <https://revealjs.com/>.
-        '';
-      };
-
-      readme-and-licence = {
-        path = ./readme-and-licence;
-        description = "Basic README and LICENSE skeletons";
-        welcomeText = ''
-          # Initialized basic README.md and LICENSE.md skeletons.
-
-          - Choose a license (compare <https://choosealicense.com/>),
-          - update the `Licence` section in `README.md`, and
-          - add the licence text to `LICENSE.md`.
-        '';
-      };
-
       python-poetry = {
         path = ./python-poetry;
         description = "A Python project with Poetry";
@@ -48,6 +26,28 @@
           # Initialized a Python project with uv.
 
           uv: <https://docs.astral.sh/uv/>
+        '';
+      };
+
+      readme-and-licence = {
+        path = ./readme-and-licence;
+        description = "Basic README and LICENSE skeletons";
+        welcomeText = ''
+          # Initialized basic README.md and LICENSE.md skeletons.
+
+          - Choose a license (compare <https://choosealicense.com/>),
+          - update the `Licence` section in `README.md`, and
+          - add the licence text to `LICENSE.md`.
+        '';
+      };
+
+      revealjs = {
+        path = ./revealjs;
+        description = "A reveal.js presentation";
+        welcomeText = ''
+          # Initialized a reveal.js presentation.
+
+          reveal.js: <https://revealjs.com/>.
         '';
       };
     };
